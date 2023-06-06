b6e9d775180819208ca9d34f9651ce19ca877935
Unien <unien7415@gmail.com>
2023-06-06 11:01:46 +0900

    データシステムを追加し

---
+++ Assets/Scripts/Player/Logic/Player.cs
@@ -0,0 +1,97 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public PlayerData_SO playerData;
+    #region PlayerData変数
+    private float maxHp
+    {
+        //playerDataの中にmaxHpをゲットして、自分の変数になる;もし探さなければ0になる
+        get { if (playerData != null) return playerData.maxHp; else return 0; }
+
+    }
+    private float hp
+    {
+        //playerDataの中にhpをゲットして、自分の変数になる;もし探さなければ0になる
+        get { if (playerData != null) return playerData.hp; else return 0; }
+
+    }
+    private float damage
+    {
+        //playerDataの中にdamageをゲットして、自分の変数になる;もし探さなければ0になる
+        get { if (playerData != null) return playerData.damage; else return 0; }
+       
+    }
+
+    private float speed
+    {
+        //playerDataの中にspeedをゲットして、自分の変数になる;もし探さなければ0になる
+        get { if (playerData != null) return playerData.speed; else return 0; }
+
+    }
+
+
+    public bool isDead
+    {
+        get { if (playerData != null) return playerData.isDead; else return false; }
+        set { playerData.isDead = value; }//setは自分の変数がplayerDataに送る
+    }
+    #endregion 
+
+    public Vector2 shotrote;
+    private GameObject Enemyobj;
+    public bool ATK;
+    private float time;
+
+    private void Awake()
+    {
+            
+    }
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        if (isDead)
+        {
+            canATK();
+        }
+        
+
+        
+    }
+
+    void canATK()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+            {
+                ATK = true;
+            }
+        if (ATK)
+        {
+            time += Time.deltaTime;
+            shotrote = new Vector2(Enemyobj.transform.position.x - this.transform.position.x, Enemyobj.transform.position.y - this.transform.position.y);
+
+            if (time >= 1)
+            {
+                time = 0;
+                ATK = false;
+            }
+        }
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.CompareTag("Enemy"))
+        {
+            Enemyobj = col.gameObject;
+        }
+    }
+}
