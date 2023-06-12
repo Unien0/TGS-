924a2cf6c1e98a48ceb712cffe5e93358dd4b82b
DESKTOP-DII4QQ9\24105 <unien7415@gmail.com>
2023-06-12 15:41:06 +0900

    データ関連、新しいボイスを更新しました

--- NingJya/Assets/Scripts/Enemy/Logic/Enemy.cs
+++ NingJya/Assets/Scripts/Enemy/Logic/Enemy.cs
@@ -4,23 +4,216 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    public PlayerData_SO playerData;
+    #region PlayerData変数
+    private float playerHp
+    {
+        //playerDataの中にhpをゲットして、自分の変数になる;もし探さなければ0になる
+        get { if (playerData != null) return playerData.hp; else return 0; }
+        set { playerData.hp = value; }//playをあたらplayerのHPを減らす
+    }
+    private float playerDamage
+    {
+        //playerDataの中にdamageをゲットして、自分の変数になる;もし探さなければ0になる
+        get { if (playerData != null) return playerData.damage; else return 0; }
+
+    }
+    private float yellowForce
+    {
+        //リズムを合わない、黄色エリアのとき
+        get { if (playerData != null) return playerData.force; else return 0; }
+    }
+    private float whiteForce
+    {
+        //リズム合うとき
+        get { if (playerData != null) return playerData.force * 4; else return 0; }
+    }
+    private float redForce
+    {
+        //赤エリアのとき
+        get { if (playerData != null) return playerData.force * 8; else return 0; }
+    }
+    #endregion 
+
+    public EnemyData_SO enemyData;
+    #region EnemyData変数
+    private float enemyHp
+    {
+        //enemyDataをゲット
+        get { if (enemyData != null) return enemyData.Hp; else return 0; }
+        
+    }
+    private float enemyDamage
+    {
+        //enemyDataをゲット
+        get { if (enemyData != null) return enemyData.damage; else return 0; }
+        set { enemyData.damage = value; }
+    }
+    private float yellowExistenceTime
+    {
+        //enemyDataをゲット
+        get { if (enemyData != null) return enemyData.existenceTime; else return 0; }
+
+    }
+    //リズムを合わせて敵を殺すれば4倍の存在時間があります
+    private float whiteExistenceTime
+    {
+        //enemyDataをゲット
+        get { if (enemyData != null) return enemyData.existenceTime*4; else return 0; }
+
+    }
+    //赤エリアで敵を殺すれば8倍の存在時間があります
+    private float redExistenceTime
+    {
+        //enemyDataをゲット
+        get { if (enemyData != null) return enemyData.existenceTime*8; else return 0; }
+
+    }
+
+
+    private bool attackable
+    {
+        //penemyDataをゲット
+        get { if (enemyData != null) return enemyData.attackable; else return false; }
+    }
+    private bool removable
+    {
+        //penemyDataをゲット
+        get { if (enemyData != null) return enemyData.removable; else return false; }
+    }
+    private bool blowable
+    {
+        //penemyDataをゲット
+        get { if (enemyData != null) return enemyData.blowable; else return false; }
+    }
+    private bool beingBlow
+    {
+        //penemyDataをゲット
+        get { if (enemyData != null) return enemyData.beingBlow; else return false; }
+    }
+    #endregion
+
+
     private Rigidbody2D rb2d;
+    private bool ATKAREA = false;
+    private GameObject PlayerObject;
+    private Vector2 moveint;
+    // moveint偺寢壥傪惓晧偺傒偺抣偵偡傞
+    private Vector2 moveit;
+    // 峴摦傑偱偺懸婡帪娫
+    private float actTime;
+    // 堏摦張棟偺掆巭
+    private bool stop = false;
+    private bool fix = false;
+    [SerializeField] private float speed;
+    public Vector2 shotrote;
+    private Vector2 shotIt;
+    private bool shoted;
+
+    private void Awake()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        PlayerObject = FindObjectOfType<Player>().gameObject;
+    }
 
     void Start()
     {
-        rb2d = GetComponent<Rigidbody2D>();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (FindObjectOfType<Player>().ATK)
+        actTime += Time.deltaTime;
+
+        // 擇昩宱夁帪
+        if (actTime >= 2.0f)
         {
-            rb2d.AddForce(FindObjectOfType<Player>().shotrote,ForceMode2D.Impulse);
+            // 掆巭張棟偑峴傢傟偰偄側偄側傜
+            if (!stop)
+            {
+                // 堏摦偡傞
+                moveint = new Vector2(PlayerObject.transform.position.x - transform.position.x, PlayerObject.transform.position.y - transform.position.y);
+                moveit.x = Mathf.Sign(moveint.x);
+                moveit.y = Mathf.Sign(moveint.y);
+                rb2d.velocity = moveit * 5;
+
+                // 掆巭丒弶婜壔偡傞
+                if (actTime >= 2.25f)
+                {
+                    rb2d.velocity = Vector2.zero;
+                    rb2d.angularVelocity = 0;
+                    actTime = 0;
+                }
+            }
+            else actTime = 0; ;
         }
-        else
+
+        // 僾儗僀儎乕偺峌寕斖埻偵偄傞偲偒
+        if (ATKAREA)
         {
-            rb2d.velocity = Vector3.zero;
+            // 僾儗僀儎乕偑峌寕擖椡傪偟偨帪
+            if (FindObjectOfType<Player>().ATK)
+            {
+                // 堏摦張棟傪峴傢側偄傛偆偵偡傞
+                stop = true;
+                fix = false;
+                if (!shoted)
+                {
+                    // 傆偭偲偽偡
+                    shoted = true;
+                    shotrote = new Vector2(this.transform.position.x - PlayerObject.transform.position.x, this.transform.position.y - PlayerObject.transform.position.y);
+                    shotIt.x = Mathf.Sign(shotrote.x);
+                    shotIt.y = Mathf.Sign(shotrote.y);
+                    rb2d.AddForce(shotrote * 10, ForceMode2D.Impulse);
+                }
+
+            }
+        }
+
+        // 傆偭偲偽偟掆巭張棟
+        // 峌寕擖椡偑偝傟偰偄側偄忋
+        if (FindObjectOfType<Player>().ATK == false)
+        {
+            stop = false;
+            // 廋惓張棟偑偝傟偰偄側偄側傜掆巭偡傞丅
+            if (!fix)
+            {
+                transform.eulerAngles = Vector3.zero;
+                fix = true;
+                shoted = false;
+                rb2d.velocity = Vector2.zero;
+                rb2d.angularVelocity = 0;
+            }
+        }
+    }
+
+    /// <summary>
+    /// 敵を吹っ飛ばすエリアの判定
+    /// </summary>
+    private void BlowAway()
+    {
+        //1、敵の位置が黄色、白、赤のエリア内にあるかどうかを判断する
+        //2、プレイヤーは攻撃したかを判断する
+        //3、プレイヤーとの位置によって吹っ飛ばす方向を決める
+        //4、現在位置に基づいて吹っ飛ばすの力と保存時間を判断します
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        // 僾儗僀儎乕偺峌寕斖埻偵擖偭偨応崌
+        if (col.CompareTag("Player"))
+        {
+            ATKAREA = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D col)
+    {
+        // 僾儗僀儎乕偺峌寕斖埻偐傜敳偗偨応崌
+        if (col.CompareTag("Player"))
+        {
+            ATKAREA = false;
         }
     }
 }
--- NingJya/Assets/Scripts/Player/Logic/Player.cs
+++ NingJya/Assets/Scripts/Player/Logic/Player.cs
@@ -24,30 +24,42 @@ public class Player : MonoBehaviour
         get { if (playerData != null) return playerData.damage; else return 0; }
        
     }
-
     private float speed
     {
         //playerDataの中にspeedをゲットして、自分の変数になる;もし探さなければ0になる
         get { if (playerData != null) return playerData.speed; else return 0; }
 
     }
-
-
+   
     public bool isDead
     {
         get { if (playerData != null) return playerData.isDead; else return false; }
         set { playerData.isDead = value; }//setは自分の変数がplayerDataに送る
     }
+    public bool attackable
+    {
+        get { if (playerData != null) return playerData.attackable; else return false; }
+        set { playerData.attackable = value; }//setは自分の変数がplayerDataに送る
+    }
+    public bool removable
+    {
+        get { if (playerData != null) return playerData.removable; else return false; }
+        set { playerData.removable = value; }//setは自分の変数がplayerDataに送る
+    }
+
     #endregion 
 
     public Vector2 shotrote;
     private GameObject Enemyobj;
     public bool ATK;
     private float time;
+    private Rigidbody2D rb2d;
+
 
     private void Awake()
     {
-            
+        rb2d = GetComponent<Rigidbody2D>();
+
     }
 
     void Start()
@@ -58,40 +70,45 @@ public class Player : MonoBehaviour
     
     void Update()
     {
-        if (isDead)
+        if (!isDead)
         {
-            canATK();
-        }
-        
+            move();
 
-        
-    }
-
-    void canATK()
-    {
-        if (Input.GetKeyDown(KeyCode.E))
+            // 峌寕張棟
+            if (Input.GetKeyDown(KeyCode.E))
             {
                 ATK = true;
             }
-        if (ATK)
-        {
-            time += Time.deltaTime;
-            shotrote = new Vector2(Enemyobj.transform.position.x - this.transform.position.x, Enemyobj.transform.position.y - this.transform.position.y);
 
-            if (time >= 1)
+            // 峌寕張棟
+            if (ATK)
             {
-                time = 0;
-                ATK = false;
+                // 僋乕儖僟僂儞偺妋擣
+                time += Time.deltaTime;
+                if (time >= 1)
+                {
+                    time = 0;
+                    ATK = false;
+                }
             }
-        }
+        }        
     }
 
-
-    private void OnTriggerEnter2D(Collider2D col)
+    void move()
     {
-        if (col.CompareTag("Enemy"))
-        {
-            Enemyobj = col.gameObject;
-        }
+        // 堏摦擖椡 
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        // 堏摦張棟
+        rb2d.velocity = new Vector2(horizontal * speed, vertical * speed);
     }
+
+    //private void OnTriggerEnter2D(Collider2D col)
+    //{
+    //    if (col.CompareTag("Enemy"))
+    //    {
+    //        Enemyobj = col.gameObject;
+    //    }
+    //}
 }
