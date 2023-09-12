f8c958a0a7729fc7725b5af0b48236cd3eb09c13
UNIEN0 <unien7415@gmail.com>
2023-09-12 16:28:43 +0900

    対話判定修正

--- NingJya/Assets/Scripts/Objects/DialogueArea.cs
+++ NingJya/Assets/Scripts/Objects/DialogueArea.cs
@@ -6,91 +6,54 @@ using Fungus;
 public class DialogueArea : MonoBehaviour
 {
     public string chatName;
-    private bool outEnemy = false;
+    private bool chated = false;
     private bool canChat = false;
-    private bool endChat = false;
 
     private Flowchart flowchart;
     public GameObject iconDisplay;
-    //public GameObject npc1;
-
-    //public GameObject npc2;
     private Collider2D col2D;
-    private bool make;
-    private bool isSay;
     [SerializeField] private GameObject DEAD_EFECT;
+
     private void Awake()
     {
         col2D = GetComponent<Collider2D>();
         flowchart = GameObject.Find("Flowchart").GetComponent<Flowchart>();
     }
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        //iconDisplay.SetActive(false);
-    }
-
-    // Update is called once per frame
     void Update()
-    {
-        //if (outEnemy)
-        //{
-        //    if (isSay)
-        //    {
-        //        if (!make)
-        //        {
-        //            make = true;
-        //            Instantiate(DEAD_EFECT, npc1.transform.position, npc1.transform.rotation);
+    {        
+        if (chated && Input.GetKeyDown("joystick button 0") && canChat)
+        {
 
-        //        }
-        //        else
-        //        {
-        //            npc1.SetActive(false);
-        //            iconDisplay.SetActive(false);
-        //        }
-        //    }
-        //}
-        //if (canChat && Input.GetKeyDown("joystick button 0"))
-        //{
-
-        //    //対話実装したか
-        //    if (flowchart.HasBlock(chatName))
-        //    {
-        //        flowchart.ExecuteBlock(chatName);
-        //        isSay = true;
-        //        //endChat = true;
-        //    }
-        //}
+            //対話実装したか
+            if (flowchart.HasBlock(chatName))
+            {
+                flowchart.ExecuteBlock(chatName);
+            }
+        }
     }
 
-    private void OnTriggerStay2D(Collider2D col)
+    private void OnTriggerEnter2D(Collider2D col)
     {
         // プレイヤーの攻撃範囲に入っていて、
-        if (col.gameObject.name == "！Player" && !canChat)
+        if (col.gameObject.name == "！Player" && !chated)
         {
-            //iconDisplay.SetActive(true);
-            //npc1.SetActive(true);
-            canChat = true;
+            chated = true;
+            
             flowchart.ExecuteBlock(chatName);
         }
-        
-    }
-    private void OnTriggerExit2D(Collider2D col)
-    {
-        if (col.CompareTag("Player"))
+        else if (col.gameObject.name == "！Player")
         {
-            //iconDisplay.SetActive(false);
-            //outEnemy = true;
-            //canChat = true;
-
+            canChat = true;
         }
     }
-    //private void OnTriggerExit2D(Collider2D other)
-    //{
-    //    if (other.CompareTag("Enemy"))
-    //    {
-    //        outEnemy = true;
-    //    }
-    //}
+
+    private void OnTriggerExit2D(Collider2D col)
+    {
+        if (col.gameObject.name == "！Player")
+        {
+            canChat = false;
+        }
+    }
+
 }
