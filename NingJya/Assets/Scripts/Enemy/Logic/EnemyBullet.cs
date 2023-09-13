4ce4c300ab8ac6bf9f10cacbd1d14e7901b866e6
731PC-11\student <22cr0110@jec.ac.jp>
2023-09-13 16:04:28 +0900

    バレット反射処理の変更準備

--- NingJya/Assets/Scripts/Enemy/Logic/EnemyBullet.cs
+++ NingJya/Assets/Scripts/Enemy/Logic/EnemyBullet.cs
@@ -82,46 +82,13 @@ public class EnemyBullet : MonoBehaviour
 
         if (isBlow)
         {
-            BlowAway();
+            // コンダクトマネージャーから最も近い敵をサーチ
+            // その敵の方向に対してZRoteを回転したのちに直線する
+            isBlow = false;
         }
         else
         {
             rb2d.velocity = transform.up * BulletSpeed;
-        }
-    }
-
-    private void BlowAway()
-    {
-        // プレイヤーの攻撃範囲にいるとき
-        if (inPlayerAttackRange)
-        {
-            if (Ready)
-            {
-                if (conductObject != null)
-                {
-                    //方向
-                    shotrote = new Vector2(conductObject.transform.position.x - this.transform.position.x, conductObject.transform.position.y - this.transform.position.y);
-                    shotIt.x = Mathf.Sign(shotrote.x);
-                    shotIt.y = Mathf.Sign(shotrote.y);
-                    //現在位置に基づいて吹っ飛ばすの力と保存時間を判断します
-                    rb2d.AddForce(shotIt * BulletSpeed * 15);                
-                }
-                else
-                {
-                    //方向
-                    shotrote = new Vector2(this.transform.position.x - PlayerObject.transform.position.x, this.transform.position.y - PlayerObject.transform.position.y).normalized;
-                    if (shotrote.x <= -0.5f || shotrote.x >= 0.5f)
-                    { shotIt.x = Mathf.Sign(shotrote.x); }
-                    else
-                    { shotIt.x = 0; }
-                    if (shotrote.y <= -0.5f || shotrote.y >= 0.5f)
-                    { shotIt.y = Mathf.Sign(shotrote.y); }
-                    else
-                    { shotIt.y = 0; }
-                    //4、現在位置に基づいて吹っ飛ばすの力と保存時間を判断します
-                    rb2d.AddForce(shotIt * BulletSpeed * 15);
-                }
-            }
         }
     }
 
