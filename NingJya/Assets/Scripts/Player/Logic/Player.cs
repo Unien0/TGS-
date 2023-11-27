c53b8bbcb16c739ec1be1d2958dcc99d41b99caf
731PC-11\student <22cr0110@jec.ac.jp>
2023-11-27 12:26:31 +0900

    Playerスクリプト修正(短入力移動のブラッシュアップ)

--- NingJya/Assets/Scripts/Player/Logic/Player.cs
+++ NingJya/Assets/Scripts/Player/Logic/Player.cs
@@ -92,6 +92,8 @@ public class Player : MonoBehaviour
     }
     #endregion 
 
+    [SerializeField] private GameManeger gameManegerCS;
+
     private float inputX;                   // 横軸の移動入力を確認する変数
     private float inputY;                   // 縦軸の移動入力を確認する変数
     private Vector2 TemporaryInput;         // 一時的に移動入力を保存する変数
@@ -102,6 +104,7 @@ public class Player : MonoBehaviour
     private bool maked;                     // 移動エフェクトの生成を確認する変数
     // 移動エフェクトを保存する変数
     [SerializeField] private GameObject MoveEfect;
+    private bool ActFix;
 
     private float MutekiTime;               // ダメージ時後の無敵時間を管理する変数
     public bool Hit;                        // ダメージを食らった時の確認用変数
@@ -256,10 +259,11 @@ public class Player : MonoBehaviour
         // 移動入力 
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
-        if(horizontal == 0)
+
+        if (horizontal == 0)
         {
             inputFixH += Time.deltaTime;
-            if ((inputFixH > 2f) || removable)
+            if (inputFixH > gameManegerCS.OneTempo)
             {
                 TemporaryInput.x = 0;
             }
@@ -273,7 +277,7 @@ public class Player : MonoBehaviour
         if(vertical == 0)
         {
             inputFixV += Time.deltaTime;
-            if ((inputFixV > 2f) || removable)
+            if (inputFixV > gameManegerCS.OneTempo)
             {
                 TemporaryInput.y = 0;
             }
@@ -294,91 +298,125 @@ public class Player : MonoBehaviour
                     maked = true;
                     Instantiate(MoveEfect, this.transform.position, MoveEfect.transform.rotation);
                 }
+                TemporaryInput = Vector2.zero;
             }
+
             // 移動処理
             rb2d.velocity = new Vector2(moveInput.x * speed, moveInput.y * speed);
+
+
+            ActFix = false;
         }
         else
         {
-            IsAttack = false; 
-            maked = false;
-            if ((TemporaryInput.x != 0) || (TemporaryInput.y != 0))
-            {   // 入力に応じて、攻撃範囲を回転する
+            if (!ActFix)
+            {
+                ActFix = true;
 
-                #region roteMax変更
-                // 入力に応じて、回転を変更する
-                if ((TemporaryInput.x >= 0.425f) && (TemporaryInput.x <= 0.8f) && (TemporaryInput.y >= 0.01f))
-                { moveInput = new Vector2(1, 1);
-                  moveDirection = 315;}
-
-                if ((TemporaryInput.x == 0) && (TemporaryInput.y >= 0.01))
-                { moveInput = new Vector2(0, 1);
-                  moveDirection = 0; }
-
-                if ((TemporaryInput.x <= -0.425f) && (TemporaryInput.x >= -0.8f) && (TemporaryInput.y >= 0.01f))
-                { moveInput = new Vector2(-1, 1); 
-                    moveDirection = 45; }
-
-                if ((TemporaryInput.x <= -0.01f) && (TemporaryInput.y == 0))
-                { moveInput = new Vector2(-1, 0); 
-                  moveDirection = 90; }
-
-                if ((TemporaryInput.x <= -0.425f) && (TemporaryInput.x >= -0.8f) && (TemporaryInput.y <= -0.01f))
-                { moveInput = new Vector2(-1, -1); 
-                  moveDirection = 135; }
-
-                if ((TemporaryInput.x == 0) && (TemporaryInput.y <= -0.01f))
-                { moveInput = new Vector2(0, -1); 
-                  moveDirection = 180; }
-
-                if ((TemporaryInput.x >= 0.425f) && (TemporaryInput.x <= 0.8f) && (TemporaryInput.y <= -0.01f))
-                { moveInput = new Vector2(1, -1); 
-                  moveDirection = 225; }
-
-                if ((TemporaryInput.x >= 0.01f) && (TemporaryInput.y == 0))
-                { moveInput = new Vector2(1, 0);
-                  moveDirection = 270; }
-                #endregion
-
-                // 入力に応じてアニメーションを変更する
-                // 横軸入力
-                if (moveInput.x >= 0.1f)
-                {
-                    SpR.flipX = true;
-                    anim.SetBool("Side", true);
-                }
-                else if (moveInput.x <= -0.1f)
-                {
-                    SpR.flipX = false;
-                    anim.SetBool("Side", true);
-                }
-                else
-                {
-                    anim.SetBool("Side", false);
-                }
-                // 縦軸入力
-                if (moveInput.y >= 0.1f)
-                {
-                    anim.SetBool("Top", true);
-                    anim.SetBool("Bottom", false);
-
-                }
-                else if (moveInput.y <= -0.1f)
-                {
-                    anim.SetBool("Top", false);
-                    anim.SetBool("Bottom", true);
-                }
-                else
-                {
-                    anim.SetBool("Top", false);
-                    anim.SetBool("Bottom", false);
-                }
+                // 再度アクション処理ができるように初期化する
+                IsAttack = false;                             // 攻撃処理の初期化
+                maked = false;                                // 移動エフェクト生成フラグの初期化
+                rb2d.velocity = new Vector2(0, 0);            // キャラの移動を停止させる
+                attackable = false;                           // 攻撃許可の取り消し
+                anim.SetBool("Attack", false);                // 攻撃アニメーションの停止
+                anim.SetBool("Attack", false);                // 攻撃アニメーションの停止
             }
             else
-            {moveInput = Vector2.zero; }
-            rb2d.velocity = new Vector2(0, 0);
-            attackable = false;
-            anim.SetBool("Attack", false);
+            {
+                #region 移動入力の処理 　
+                if ((TemporaryInput.x != 0) || (TemporaryInput.y != 0))
+                {   // 入力に応じて、攻撃範囲を回転する
+                    #region roteMax変更
+                    // 入力に応じて、回転を変更する
+                    if ((TemporaryInput.x >= 0.425f) && (TemporaryInput.x <= 0.8f) && (TemporaryInput.y >= 0.01f))
+                    {
+                        moveInput = new Vector2(1, 1);
+                        moveDirection = 315;
+                    }
+
+                    if ((TemporaryInput.x == 0) && (TemporaryInput.y >= 0.01))
+                    {
+                        moveInput = new Vector2(0, 1);
+                        moveDirection = 0;
+                    }
+
+                    if ((TemporaryInput.x <= -0.425f) && (TemporaryInput.x >= -0.8f) && (TemporaryInput.y >= 0.01f))
+                    {
+                        moveInput = new Vector2(-1, 1);
+                        moveDirection = 45;
+                    }
+
+                    if ((TemporaryInput.x <= -0.01f) && (TemporaryInput.y == 0))
+                    {
+                        moveInput = new Vector2(-1, 0);
+                        moveDirection = 90;
+                    }
+
+                    if ((TemporaryInput.x <= -0.425f) && (TemporaryInput.x >= -0.8f) && (TemporaryInput.y <= -0.01f))
+                    {
+                        moveInput = new Vector2(-1, -1);
+                        moveDirection = 135;
+                    }
+
+                    if ((TemporaryInput.x == 0) && (TemporaryInput.y <= -0.01f))
+                    {
+                        moveInput = new Vector2(0, -1);
+                        moveDirection = 180;
+                    }
+
+                    if ((TemporaryInput.x >= 0.425f) && (TemporaryInput.x <= 0.8f) && (TemporaryInput.y <= -0.01f))
+                    {
+                        moveInput = new Vector2(1, -1);
+                        moveDirection = 225;
+                    }
+
+                    if ((TemporaryInput.x >= 0.01f) && (TemporaryInput.y == 0))
+                    {
+                        moveInput = new Vector2(1, 0);
+                        moveDirection = 270;
+                    }
+                    #endregion
+
+                    // 入力に応じてアニメーションを変更する
+                    #region アニメーション変更 
+                    // 横軸入力
+                    if (moveInput.x >= 0.1f)
+                    {
+                        SpR.flipX = true;
+                        anim.SetBool("Side", true);
+                    }
+                    else if (moveInput.x <= -0.1f)
+                    {
+                        SpR.flipX = false;
+                        anim.SetBool("Side", true);
+                    }
+                    else
+                    {
+                        anim.SetBool("Side", false);
+                    }
+                    // 縦軸入力
+                    if (moveInput.y >= 0.1f)
+                    {
+                        anim.SetBool("Top", true);
+                        anim.SetBool("Bottom", false);
+
+                    }
+                    else if (moveInput.y <= -0.1f)
+                    {
+                        anim.SetBool("Top", false);
+                        anim.SetBool("Bottom", true);
+                    }
+                    else
+                    {
+                        anim.SetBool("Top", false);
+                        anim.SetBool("Bottom", false);
+                    }
+                    #endregion
+                }
+                else
+                { moveInput = Vector2.zero; }
+                #endregion
+            }
         }
     }
 
